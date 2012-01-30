3f94811558f06b9e5df7f6c6262f13b6131ecfce
Wijnand Modderman-Lenstra <maze@pyth0n.org>
2012-01-30 17:10:07 +0100

    Do not initiate context stack as a list from parameter, do it in the class

--- nagios_cli/context.py
+++ nagios_cli/context.py
@@ -1,20 +1,36 @@
 class Context(object):
-    def __init__(self, stack=[]):
-        self.stack = stack
+    def __init__(self, stack=None):
+        if stack is None:
+            self.stack = []
+        else:
+            self.stack = stack
 
     def add(self, item):
+        '''
+        Add an object to the stack.
+        '''
         self.stack.append(item)
 
     def pop(self, item=-1, default=None):
+        '''
+        Remove the current active (lastly added) object from the stack.
+        Returns the removed object (if any).
+        '''
         if self.stack:
             return self.stack.pop(item)
         else:
             return default
 
     def get(self, item=-1):
+        '''
+        Get the current active (lastly added) object from the stack.
+        '''
         return self.stack and self.stack[item]
 
     def set(self, item):
+        '''
+        Reset the stack to this single item.
+        '''
         self.stack = [item]
 
 
